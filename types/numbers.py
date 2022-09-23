0d9379fb36b45d827cbebf3f54d8d95f2e9e0320
tokami3d <dzholdoshev1@gmail.com>
2022-09-23 20:21:46 +0600

    арифметика

---
+++ types/numbers.py
@@ -0,0 +1,34 @@
+
+# def new_func():
+#     print (pow(5, 2, 5))
+
+# # divmod(a, b) -она выводит два числа первое число это результат целочисленного деление(//) а второе число это модульное деление (%)двух чисел 
+
+#     res = divmod(3234, 9)
+#     print (res)
+
+# new_func()
+
+# res = 44/33
+# print(round(res, 1))
+
+# abs - абсолютное значение числа 
+
+# print(abs(-101))
+
+import math
+# print(math.pi)
+
+# math.sqrt - корень числа 
+
+# print(math.sqrt(25))
+# print(math.sqrt(2566))
+# print(math.sqrt(896))
+
+from math import pi
+
+# print (pi)
+
+from math import sqrt
+
+print(sqrt(88472947))
